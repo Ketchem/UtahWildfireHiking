159791a1f8dd14b23032e7bdde257664dd1d9b84
Alex Gilvarry <gilvarry@wisc.edu>
2018-12-08 09:43:20 -0700

    js and css
    js and css

---
+++ web/js/loadmap.js
@@ -0,0 +1,56 @@
+
+var map;
+
+function initialization() {
+    showAllReports();
+}
+
+function showAllReports() {
+    $.ajax({
+        url: 'HttpServlet',
+        type: 'POST',
+        data: { "tab_id": "1"},
+        success: function(reports) {
+            mapInitialization(reports);
+        },
+        error: function(xhr, status, error) {
+            alert("An AJAX error occured: " + status + "\nError: " + error);
+        }
+    });
+}
+
+function mapInitialization(reports) {
+    var mapOptions = {
+        mapTypeId : google.maps.MapTypeId.ROADMAP, // Set the type of Map
+    };
+
+    // Render the map within the empty div
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    var bounds = new google.maps.LatLngBounds ();
+
+    $.each(reports, function(i, e) {
+        var long = Number(e['longitude']);
+        var lat = Number(e['latitude']);
+        var latlng = new google.maps.LatLng(lat, long);
+
+        bounds.extend(latlng);
+
+        // Create the marker
+        var marker = new google.maps.Marker({ // Set the marker
+            position : latlng, // Position marker to coordinates
+            map : map, // assign the market to our map variable
+        });
+
+    });
+
+    map.data.loadGeoJson('SmallTrails.geojson');
+
+
+    map.fitBounds (bounds);
+
+}
+
+
+//Execute our 'initialization' function once the page has loaded.
+google.maps.event.addDomListener(window, 'load', initialization);
